e0176f5874070ec7e607a97d50ac0fd1123165d2
Nicolas DUBIEN <github@dubien.org>
2024-12-03 19:24:21 +0000

    Basics

---
+++ src/App.spec.jsx
@@ -0,0 +1,8 @@
+import { test } from "vitest";
+import { render } from "@testing-library/react";
+import React from "react";
+import App from "./App";
+
+test("without stubs", () => {
+  render(<App />);
+});
